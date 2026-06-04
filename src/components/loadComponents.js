3f66f72ab2de036cb22873d7a310ce8954d809e3
RomanSedlo <roman.sedlar.024@pslib.cz>
2026-06-05 01:29:04 +0200

    WEB v0.3m

--- src/components/loadComponents.js
+++ src/components/loadComponents.js
@@ -4,5 +4,5 @@ async function loadComponent(selector, path) {
   const html = await fetch(path).then(part => part.text());
   document.querySelector(selector).innerHTML = html;
 }
-await loadComponent('#navbar', 'src/components/navbar.html');
-await loadComponent('#footer', 'src/components/footer.html');
+await loadComponent('#navbar', import.meta.env.BASE_URL + '/src/components/navbar.html');
+await loadComponent('#footer', import.meta.env.BASE_URL + '/src/components/footer.html');
--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
 import '/src/style.css';
+
+document.querySelector(".menu__button").addEventListener('click', () => document.querySelector(".nav--primary").classList.toggle("visible"))
