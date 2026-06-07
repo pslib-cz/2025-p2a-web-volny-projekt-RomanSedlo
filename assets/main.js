6ebfdb93d46dc44291f40733cac25d5609ec6e66
RomanSedlo <roman.sedlar.024@pslib.cz>
2026-06-07 21:53:45 +0200

    WEB v0.4.4m

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    async function loadComponent(selector, path) {
-        const html = await fetch(path).then(part => part.text());
-        document.querySelector(selector).innerHTML = html;
-    }
-    if(document.querySelector('#navbar') || document.querySelector('#footer')) {
-        await loadComponent('#navbar', '../assets/components/navbar.html');
-        await loadComponent('#footer', '../assets/components/footer.html');
-    }
+const base = new URL('../', import.meta.url).href;
 
-    document.querySelector(".menu__button").addEventListener('click', () => {
-        document.querySelector(".nav--primary").classList.toggle("visible")
-        document.querySelector(".menu__button").classList.toggle("menu__button--visible")
-    })
+async function loadComponent(selector, path) {
+    const html = await fetch(path).then(part => part.text());
+    document.querySelector(selector).innerHTML = html;
+}
+if (document.querySelector('#navbar') || document.querySelector('#footer')) {
+    await loadComponent('#navbar', '/assets/components/navbar.html');
+    await loadComponent('#footer', '/assets/components/footer.html');
+}
+
+document.querySelector(".menu__button").addEventListener('click', () => {
+    document.querySelector(".nav--primary").classList.toggle("visible")
+    document.querySelector(".menu__button").classList.toggle("menu__button--visible")
 })
