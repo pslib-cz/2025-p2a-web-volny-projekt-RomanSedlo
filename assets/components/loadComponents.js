89d9f19f7058aa94a709296eea7cbc60a9f5a50b
RomanSedlo <roman.sedlar.024@pslib.cz>
2026-06-07 12:22:18 +0200

    WEB v0.4m

--- assets/components/loadComponents.js
+++
@@ -1,8 +0,0 @@
-import './navbar.css';
-import './footer.css';
-async function loadComponent(selector, path) {
-  const html = await fetch(path).then(part => part.text());
-  document.querySelector(selector).innerHTML = html;
-}
-await loadComponent('#navbar', import.meta.env.BASE_URL + '/src/components/navbar.html');
-await loadComponent('#footer', import.meta.env.BASE_URL + '/src/components/footer.html');
